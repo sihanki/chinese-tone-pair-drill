0466118b282d12c113ac121b888997f2bd23477f
Igor Novikov <ni7529@yandex.ru>
2026-08-06 13:14:57 +0300

    Change confirmation message

--- src/components/QuizScreen.tsx
+++ src/components/QuizScreen.tsx
@@ -91,9 +91,17 @@ export default function QuizScreen({ questions, onFinish, onQuit }: Props) {
         <span className="muted">
           Question {index + 1} / {total}
         </span>
-        <button type="button" className="btn ghost small" onClick={() => {
-          if (window.confirm('Quit the drill?')) onQuit(results)
-        }}>
+        <button
+          type="button"
+          className="btn ghost small"
+          onClick={() => {
+            if (results.length === 0) {
+              onQuit(results)
+            } else if (window.confirm('Quit the drill?\nYou will be redirected to the results screen.')) {
+              onQuit(results)
+            }
+          }}
+        >
           Quit
         </button>
       </div>
